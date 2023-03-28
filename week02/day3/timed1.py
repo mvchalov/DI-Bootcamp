3e66edf0b4c53bc535873200bf75c5ab97dcfffc
Max Chalov <mvchalov@gmail.com>
2023-03-28 20:58:49 +0300

    Week 02. Day 3. Timed Challenge #1

---
+++ week02/day3/timed1.py
@@ -0,0 +1,12 @@
+import functools
+
+num = int(input("Enter the number: "))
+divisors = []
+for i in range(1, num):
+    if num % i == 0:
+        divisors.append(i)
+if functools.reduce(lambda a, e: a + e, divisors, 0) == num:
+    print('True')
+else:
+    print('False')
+    
